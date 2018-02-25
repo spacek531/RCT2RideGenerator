69bdef10d416388551b838ee3130c1a3abeeeda9
spacek531 <spacek531@comcast.net>
2018-02-24 20:15:44 -0800

    trying to decide on a name for this file

--- src/newguistuff/colorschemes.c
+++ src/newguistuff/colorschemes.c
@@ -1,4 +1,4 @@
-#include "colordialog.h"
+#include "colorschemes.h"
 #include "palette.h"
 #include "image.h"
 #include <stdlib.h>
@@ -7,14 +7,14 @@
 
 static void color_dialog_tool_clicked(GtkWidget* widget, gpointer user_data)
 {
-    color_select_tool_t* tool = (color_select_tool_t*)user_data;
+    color_dialog_tool_t* tool = (color_dialog_tool_t*)user_data;
     if (tool->value != NULL)
         gtk_dialog_response(tool->dialog->dialog,tool->value);
 }
 
 color_dialog_tool_t* color_dialog_tool_new(uint8_t color)
 {
-    color_select_tool_t* tool = malloc(sizeof(color_select_tool_t));
+    color_dialog_tool_t* tool = malloc(sizeof(color_dialog_tool_t));
     tool->value = color;
     tool->pixbuf = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, 20, 20);
     pixbuf_set_color(tool->pixbuf, palette_remap_section_index(color, 6));
@@ -36,30 +36,36 @@ void color_dialog_tool_free(color_dialog_tool_t* tool)
 void color_dialog_free(color_dialog_t* dialog)
 {
     int i;
-    for (i = 0; i < 32; i++) {
-        color_select_tool_free(dialog->tools[i]);
+    for (i = 0; i < NUM_CHOOSABLE_COLORS; i++) {
+        color_dialog_tool_free(dialog->tools[i]);
     }
-    gtk_widget_destroy(dialog->container);
     free(dialog->tools);
+    gtk_widget_destroy(dialog->container);
+    gtk_widget_destroy(dialog->dialog);
     free(dialog);
 }
 
-color_dialog_new(char* colorName)
+void color_dialog_free_connection(GtkWidget* widget, gpointer user_data)
 {
+    color_dialog_t* dialog = (color_dialog_t*)user_data;
+    color_dialog_free(dialog);
+}
+
+color_dialog_t* color_dialog_new(char* colorName)
+{
+    int gridHeight = NUM_CHOOSABLE_COLORS/COLORGRID_WIDTH + (NUM_CHOOSABLE_COLORS % COLORGRID_WIDTH != 0)
     color_dialog_t* dialog = malloc(sizeof(color_dialog_t));
     dialog->dialog = gtk_dialog_new_with_buttons(colorName, NULL, 0, "Cancel",GTK_RESPONSE_CANCEL, NULL);
     GtkWidget* content_area = gtk_dialog_get_content_area(GTK_DIALOG(dialog->dialog));
-    dialog->tools = malloc(32 * sizeof(color_select_tool_t*));
-    dialog->container = gtk_table_new(4,8,TRUE);
-    for (i=0; i < 32, i++) {
+    GtkWidget* grid = gtk_table_new(gridHeight,COLORGRID_WIDTH,TRUE);
+    dialog->tools = malloc(NUM_CHOOSABLE_COLORS * sizeof(color_dialog_tool_t*));
+    for (i=0; i < NUM_CHOOSABLE_COLORS, i++) {
         dialog->tools[i] = color_dialog_tool_new(i,colorAddress);
         dialog->tools[i]->dialog = dialog;
-        gtk_table_attach_defaults(GTK_TABLE(dialog->container),dialog->tools[i],i%8,(i%8)+1,i/4,i/4+1);
+        gtk_table_attach_defaults(GTK_TABLE(dialog->container),dialog->tools[i],i%COLORGRID_WIDTH,(i%COLORGRID_WIDTH)+1,i/gridHeight,i/gridHeight+1);
     }
     gtk_box_pack_start(GTK_BOX(content_area), dialog->container, FALSE, FALSE, 2);
-    
-    g_signal_connect_swapped (dialog,
-        "response",
-        G_CALLBACK (gtk_widget_destroy),
-        dialog);
+    g_signal_connect(dialog->dialog, "response",
+        G_CALLBACK(color_dialog_free_connection), dialog);
+    return dialog;
 }
---
+++ src/newguistuff/colorschemes.h
@@ -0,0 +1,43 @@
+#ifndef COLORSCHEMES_INCLUDED
+#define COLORSCHEMES_INCLUDED
+
+#include "dat.h"
+#include "modeldialog.h"
+#include "interface.h"
+#include <gtk/gtk.h>
+#define NUM_CHOOSABLE_COLORS 32
+#define COLORGRID_WIDTH 8
+
+typedef struct {
+    uint8_t value;
+    GdkPixbuf* pixbuf;
+    GtkWidget* image;
+    GtkToolItem* tool_item;
+    color_dialog_t* dialog;
+} color_dialog_tool_t;
+
+typedef struct {
+    GtkWidget* dialog;
+    uint8_t* colorAddress;
+    color_dialog_tool_t** tools;
+} color_dialog_t;
+
+typedef struct {
+    uint8_t* color;
+    GdkPixbuf* pixbuf;
+    GtkWidget* image;
+    GtkToolItem* tool_item;
+    color_dialog_t* dialog;
+} color_scheme_tool_t;
+
+typedef struct {
+    color_scheme_t* color_scheme;
+    uint8_t index;
+    GtkWidget* hbox;
+    color_scheme_tool_t** tools;
+} color_scheme_widget_t;
+
+color_dialog_t* color_dialog_new(char* colorName, uint8_t* colorAddress);
+void color_dialog_free(color_dialog_t* dialog);
+
+#endif //COLORSCHEMES_INCLUDED
